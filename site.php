1c134dc8505c098968100590a6998e2caee2e6a1
sky107 <69970001+sky107@users.noreply.github.com>
2020-08-22 01:25:46 +0530

    Add files via upload

---
+++ site.php
@@ -0,0 +1,30 @@
+<?php
+if($_POST['name'])
+{
+$server="localhost";
+$username="root";
+$password="";
+$dbname="ndb";
+$con=mysqli_connect($server,$username,$password,$dbname);
+if(!$con)
+{
+    die("Connection Failed : ".mysqli_connect_error());
+}
+$name=$_POST['name'];
+$email=$_POST['email'];
+$phone=$_POST['phone'];
+$sql="INSERT INTO final(name,email,phone)
+VALUES('$name','$email','$phone');";
+if($con->query($sql)==true)
+{   
+   echo "Your Data has been successfully submitted";
+}
+else
+{
+    echo "Error $sql<br> con->error";
+}
+
+$con->close();
+}
+else
+header("location:index.html");
